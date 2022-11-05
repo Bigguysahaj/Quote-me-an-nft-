3d97934859ae091756b536a1003342a6479affa8
bigguysahaj <singhsahaj2001@gmail.com>
2022-11-05 14:30:31 +0530

    api key not present

---
+++ generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from "openai";
+import { writeFileSync } from "fs";
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+const prompt = 'Two long roads diverging into a forest during the autumn season';
+// taken from "two roads diverged in a yellow woods" by Robert Frost
+
+const result = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "512x512"
+});
+
+const url = result.data.data[0].url;
+console.log(url);
+
+const imgResult = await fetch(url);
+const blob = await imgResult.blob();
+const buffer = Buffer.from(await blob.arrayBuffer());
+writeFileSync('./img/${Date.now()}.png', buffer);
+
